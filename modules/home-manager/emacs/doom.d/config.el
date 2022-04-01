56640525e0477e33cd5a373812586a237cd65bb1
Ahmet Cemal Özgezer <ahmet.ozgezer@siemens.com>
2022-04-01 16:02:26 +0300

    simplify pop-up rules

--- modules/home-manager/emacs/doom.d/config.el
+++ modules/home-manager/emacs/doom.d/config.el
@@ -144,30 +144,8 @@
       "M-l" #'downcase-dwim
       "M-u" #'upcase-dwim)
 
-(set-popup-rules! '(("^\\*Customize.*" :slot 2 :side right :modeline nil :select t :quit t)
-                    (" \\*undo-tree\\*" :slot 2 :side left :size 20 :modeline nil :select t :quit t)
-                    ("^\\*Password-Store" :side left :size 0.25)
-
-                    ;; * help
-                    ("^\\*info.*" :size 82 :side right :ttl t :select t :quit t)
-                    ("^\\*Man.*" :size 82 :side right :ttl t :select t :quit t)
-                    ("^\\*tldr\\*" :size 82 :side right :select t :quit t)
-                    ("^\\*helpful.*" :size 82 :side right :select t :quit t)
-                    ("^\\*Help.*" :size 82 :height 0.6 :side right :select t :quit t)
-                    ("^ \\*Metahelp.*" :size 82 :side right :select t :quit t)
-                    ("^\\*Apropos.*" :size 82 :height 0.6 :side right :select t :quit t)
-                    ("^\\*Messages\\*" :vslot -10 :height 10 :side bottom :select t :quit t :ttl nil)
-
-                    ;; ("^ ?\\*NeoTree" :side ,neo-window-position :width ,neo-window-width :quit 'current :select t)
-                    ("\\*VC-history\\*" :slot 2 :side right :size 82 :modeline nil :select t :quit t)
-
-                    ;; * web
-                    ("^\\*eww.*" :size 82 :side right :select t :quit t)
-                    ("\\*xwidget" :side right :size 100 :select t)
-
-                    ;; * lang
-                    ;; ** python
-                    ("^\\*Anaconda\\*" :side right :size 82 :quit t :ttl t)))
+(set-popup-rules! '(("^\\*info\\*" :size 82 :side right :select t :quit t)
+                    ("^\\*\\(?:Wo\\)?Man " :size 82 :side right :select t :quit t)))
 
 (after! avy
   (setq avy-all-windows 'all-frames))
