ccc82de21ba262a717168b19a53827bb9c43ad56
Ahmet Cemal Ozgezer <ozgezer@gmail.com>
2022-04-02 15:27:21 +0300

    use kitty from homebrew

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -3,7 +3,7 @@ let theme = builtins.readFile ./theme.conf;
 in
 {
   programs.kitty = {
-    enable = true;
+    enable = false;
     font = {
       package = pkgs.jetbrains-mono;
       name = "JetBrains Mono";
