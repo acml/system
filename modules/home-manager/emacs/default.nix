a11655c38d764dcc47011479b9c33d3256b1d1c3
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2022-06-18 15:46:23 +0300

    switch to native version of emacs

--- modules/home-manager/emacs/default.nix
+++ modules/home-manager/emacs/default.nix
@@ -115,8 +115,8 @@ lib.mkMerge [
     programs.emacs = {
       enable = true;
       package = lib.mkMerge [
-        (lib.mkIf isLinux pkgs.emacsPgtkNativeComp)
-        (lib.mkIf isDarwin pkgs.emacsPgtkNativeComp)
+        (lib.mkIf isLinux pkgs.emacsNativeComp)
+        (lib.mkIf isDarwin pkgs.emacsNativeComp)
       ];
       extraPackages = epkgs: (with epkgs.melpaPackages; [
         pdf-tools
