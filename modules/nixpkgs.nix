d7ea15bc975e182f909cb400be8b8a7bd7c22258
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2022-09-08 21:22:55 +0300

    nix-darwin fixes

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -8,12 +8,12 @@
       keep-derivations = true
       experimental-features = nix-command flakes
     '';
-    trustedUsers = [ "${config.user.name}" "root" "@admin" "@wheel" ];
+    settings.trusted-users = [ "${config.user.name}" "root" "@admin" "@wheel" ];
     gc = {
       automatic = true;
       options = "--delete-older-than 14d";
     };
-    maxJobs = 8;
+    settings.max-jobs = 8;
     readOnlyStore = true;
     nixPath = builtins.map
       (source: "${source}=/etc/${config.environment.etc.${source}.target}") [
@@ -22,13 +22,13 @@
       "stable"
     ];
 
-    binaryCaches = [
+    settings.substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://kclejeune.cachix.org"
     ];
 
-    binaryCachePublicKeys = [
+    settings.trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "kclejeune.cachix.org-1:fOCrECygdFZKbMxHClhiTS6oowOkJ/I/dh9q9b1I4ko="
