5dbcb8a7be1181a3ad79170ecbb462667f108bdf
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2022-10-22 16:08:36 +0300

    generate darkstar hardware configuration

--- modules/hardware/darkstar.nix
+++ modules/hardware/darkstar.nix
@@ -4,21 +4,32 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports = [ ];
 
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "mptspi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0e79aab0-c525-49e2-8969-881775ee6aab";
+    device = "/dev/disk/by-uuid/c6215e6d-3baa-4681-9531-51ee78187c38";
     fsType = "ext4";
   };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/290950fc-6304-4dfb-b147-9759d19b36ff"; }];
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/8DF7-EAE7";
+    fsType = "vfat";
+  };
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/160753bb-ee1f-4967-986a-82c4548a639e"; }];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjuction
+  # with explicit per-interface declerations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens32.useDHCP = lib.mkDefault true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
