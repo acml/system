de0efd6b72d08887c6fd6d36b52ef780528ec079
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2022-11-15 23:19:17 +0300

    home-manager installations should have noto-fonts-emoji

--- modules/home-manager/emacs/default.nix
+++ modules/home-manager/emacs/default.nix
@@ -33,6 +33,7 @@ lib.mkMerge [
     home.packages = with pkgs; [
       # fonts
       emacs-all-the-icons-fonts
+      noto-fonts-emoji
       symbola
       (pkgs.nerdfonts.override {
         fonts = [
