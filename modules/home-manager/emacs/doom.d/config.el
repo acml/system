74e96d79ac901218ced4ec49bd4b3aefdba84af0
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2022-12-29 01:29:49 +0300

    new modus-theme settings

--- modules/home-manager/emacs/doom.d/config.el
+++ modules/home-manager/emacs/doom.d/config.el
@@ -118,8 +118,8 @@
                 ;; (set-fontset-font t 'symbol (font-spec :family "Noto Emoji") frame 'append)
                 ;; (load-theme 'doom-one t)
                 (if (not (display-graphic-p))
-                    (modus-themes-load-vivendi)
-                  (modus-themes-load-operandi)
+                    (load-theme 'modus-vivendi)
+                  (load-theme 'modus-operandi)
                   (set-frame-parameter (selected-frame) 'fullscreen 'maximized))))))
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
@@ -415,145 +415,8 @@ the sequences will be lost."
   (setq modus-themes-italic-constructs t
         modus-themes-bold-constructs t
         modus-themes-mixed-fonts t
-        modus-themes-subtle-line-numbers nil
-        modus-themes-intense-mouseovers nil
-        modus-themes-deuteranopia nil
-        modus-themes-tabs-accented t
         modus-themes-variable-pitch-ui t
-        modus-themes-inhibit-reload t ; only applies to `customize-set-variable' and related
-
-        modus-themes-fringes nil ; {nil,'subtle,'intense}
-
-        ;; Options for `modus-themes-lang-checkers' are either nil (the
-        ;; default), or a list of properties that may include any of those
-        ;; symbols: `straight-underline', `text-also', `background',
-        ;; `intense' OR `faint'.
-        modus-themes-lang-checkers nil
-
-        ;; Options for `modus-themes-mode-line' are either nil, or a list
-        ;; that can combine any of `3d' OR `moody', `borderless',
-        ;; `accented', a natural number for extra padding (or a cons cell
-        ;; of padding and NATNUM), and a floating point for the height of
-        ;; the text relative to the base font size (or a cons cell of
-        ;; height and FLOAT)
-        modus-themes-mode-line '(3d borderless)
-        ;; modus-themes-mode-line '(accented borderless (padding . 4) (height . 0.9))
-
-        ;; Same as above:
-        ;; modus-themes-mode-line '(accented borderless 4 0.9)
-
-        ;; Options for `modus-themes-markup' are either nil, or a list
-        ;; that can combine any of `bold', `italic', `background',
-        ;; `intense'.
-        modus-themes-markup '(background italic)
-
-        ;; Options for `modus-themes-syntax' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
-        modus-themes-syntax '(faint)
-
-        ;; Options for `modus-themes-hl-line' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `accented', `underline', `intense'
-        modus-themes-hl-line nil
-
-        ;; Options for `modus-themes-paren-match' are either nil (the
-        ;; default), or a list of properties that may include any of those
-        ;; symbols: `bold', `intense', `underline'
-        modus-themes-paren-match '(bold intense)
-
-        ;; Options for `modus-themes-links' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
-        ;; `bold', `italic', `background'
-        modus-themes-links '(neutral-underline)
-
-        ;; Options for `modus-themes-box-buttons' are either nil (the
-        ;; default), or a list that can combine any of `flat', `accented',
-        ;; `faint', `variable-pitch', `underline', `all-buttons', the
-        ;; symbol of any font weight as listed in `modus-themes-weights',
-        ;; and a floating point number (e.g. 0.9) for the height of the
-        ;; button's text.
-        modus-themes-box-buttons '(variable-pitch flat faint 0.9)
-
-        ;; Options for `modus-themes-prompts' are either nil (the
-        ;; default), or a list of properties that may include any of those
-        ;; symbols: `background', `bold', `gray', `intense', `italic'
-        modus-themes-prompts nil
-
-        ;; The `modus-themes-completions' is an alist that reads three
-        ;; keys: `matches', `selection', `popup'.  Each accepts a nil
-        ;; value (or empty list) or a list of properties that can include
-        ;; any of the following (for WEIGHT read further below):
-        ;;
-        ;; `matches' - `background', `intense', `underline', `italic', WEIGHT
-        ;; `selection' - `accented', `intense', `underline', `italic', `text-also' WEIGHT
-        ;; `popup' - same as `selected'
-        ;; `t' - applies to any key not explicitly referenced (check docs)
-        ;;
-        ;; WEIGHT is a symbol such as `semibold', `light', or anything
-        ;; covered in `modus-themes-weights'.  Bold is used in the absence
-        ;; of an explicit WEIGHT.
-        modus-themes-completions '((matches . (extrabold))
-                                   (selection . (semibold accented))
-                                   (popup . (accented intense)))
-
-        modus-themes-mail-citations 'faint ; {nil,'intense,'faint,'monochrome}
-
-        ;; Options for `modus-themes-region' are either nil (the default),
-        ;; or a list of properties that may include any of those symbols:
-        ;; `no-extend', `bg-only', `accented'
-        modus-themes-region '(bg-only no-extend)
-
-        ;; Options for `modus-themes-diffs': nil, 'desaturated, 'bg-only
-        modus-themes-diffs 'desaturated
-
-        modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
-
-        modus-themes-org-agenda ; this is an alist: read the manual or its doc string
-        '((header-block . (variable-pitch 1.3))
-          (header-date . (grayscale workaholic bold-today 1.1))
-          (event . (accented varied))
-          (scheduled . uniform)
-          (habit . traffic-light))
-
-        ;; modus-themes-headings ; this is an alist: read the manual or its doc string
-        ;; '((1 . (overline background variable-pitch 1.3))
-        ;;   (2 . (rainbow overline 1.1))
-        ;;   (t . (semibold)))
-        )
-
-  ;; Load the theme files before enabling a theme
-  (modus-themes-load-themes)
-
-  :config
-  ;; Load the theme of your choice
-  ;; (modus-themes-load-operandi)
-  ;; ;; OR
-  ;; (load-theme 'modus-operandi t)
-  (with-eval-after-load 'pdf-tools
-      ;; Configure PDF page colors. The code below comes from Modus
-     (defun my-pdf-tools-backdrop ()
-       (face-remap-add-relative
-        'default
-        `(:background ,(modus-themes-color 'bg-alt))))
-
-     (defun my-pdf-tools-midnight-mode-toggle ()
-       (when (derived-mode-p 'pdf-view-mode)
-         (if (eq (car custom-enabled-themes) 'modus-vivendi)
-             (pdf-view-midnight-minor-mode 1)
-           (pdf-view-midnight-minor-mode -1))
-         (my-pdf-tools-backdrop)))
-
-     (defun my-pdf-tools-themes-toggle ()
-       (mapc
-        (lambda (buf)
-          (with-current-buffer buf
-            (my-pdf-tools-midnight-mode-toggle)))
-        (buffer-list)))
-
-     (add-hook 'pdf-tools-enabled-hook #'my-pdf-tools-midnight-mode-toggle)
-     (add-hook 'modus-themes-after-load-theme-hook #'my-pdf-tools-themes-toggle))
+        modus-themes-custom-auto-reload t)
   :bind ("<f5>" . modus-themes-toggle))
 
 (use-package! nov
