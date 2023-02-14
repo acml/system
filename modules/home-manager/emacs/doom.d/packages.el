04f10ce236cba393c4aa27fea06d601df4ff07a5
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2023-02-14 11:46:14 +0300

    pin transient and with-editor

--- modules/home-manager/emacs/doom.d/packages.el
+++ modules/home-manager/emacs/doom.d/packages.el
@@ -77,6 +77,9 @@
 ;; defined in multiple locations. This will use pdf-tools installed by nix
 (package! pdf-tools :built-in 'prefer)
 
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
+
 ;; (package! shrface :recipe (:host github :repo "chenyanming/shrface"))
 (package! turkish)
 
